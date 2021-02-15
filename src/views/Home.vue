2fb665a0379eccd789fbe695d68e31c76623eb01
Hanib2404 <yusufibro24@gmail.com>
2021-02-15 23:19:42 +0700

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroShayna />
+    <WomenBanner />
+    <InstaShayna />
+    <PartnerLogo />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import HeroShayna from '@/components/HeroShayna.vue'
+import WomenBanner from '@/components/WomenBanner.vue'
+import InstaShayna from '@/components/InstaShayna.vue'
+import PartnerLogo from '@/components/PartnerLogo.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld
+    HeaderShayna,
+    HeroShayna,
+    WomenBanner,
+    InstaShayna,
+    PartnerLogo,
+    Footer
+  }
+}
+</script>
